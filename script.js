a1f78a2f99afe25b8cf994ede3b86be66669f692
lara617 <146827697+lara617@users.noreply.github.com>
2023-11-29 12:48:20 +0000

    texto + imagem + modificação style

--- script.js
+++ script.js
@@ -2,8 +2,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const modeToggle = document.getElementById('mode-toggle');
     const body = document.body;
 
+
+    
     modeToggle.addEventListener('click', function () {
         body.classList.toggle('dark-mode');
         body.classList.toggle('light-mode');
     });
 });
+
+
+// script.js
+$(document).ready(function() {
+    $(".hover-zoom").mouseenter(function() {
+      $(this).css("transform", "scale(1.2) rotate(10deg)");
+    }).mouseleave(function() {
+      $(this).css("transform", "scale(1) rotate(0deg)");
+    });
+  });
+  
+
+  //$(document).ready(function() {
+    // Adiciona a classe 'show' quando a seção entra no viewport
+    //$(window).scroll(function() {
+     // $('.fade').each(function() {
+       // var elementBottom = $(this).offset().top + $(this).outerHeight();
+       // var viewportBottom = $(window).scrollTop() + $(window).height();
+       // if (elementBottom < viewportBottom) {
+      //    $(this).addClass('show');
+     //   }
+  //    });
+    //});
+  //});
+  
